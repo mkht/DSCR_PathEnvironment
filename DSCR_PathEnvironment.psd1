dc3f113a0405c59c3f45d522812bf48b8ed9a1a3
mkht <mkht@users.noreply.github.com>
2018-07-19 12:48:00 +0900

    initial commit

---
+++ DSCR_PathEnvironment.psd1
@@ -0,0 +1,60 @@
+﻿#
+# モジュール 'DSCR_PathEnvironment' のモジュール マニフェスト
+#
+# 生成者: mkht
+#
+# 生成日: 2016/11/30
+#
+
+@{
+
+# このマニフェストに関連付けられているスクリプト モジュール ファイルまたはバイナリ モジュール ファイル。
+# RootModule = ''
+
+# このモジュールのバージョン番号です。
+ModuleVersion = '0.1.0'
+
+# このモジュールを一意に識別するために使用される ID
+GUID = 'e66f633f-9ae7-4ecc-8b5b-18f1f2bc033b'
+
+# このモジュールの作成者
+Author = 'mkht'
+
+# このモジュールの会社またはベンダー
+CompanyName = ''
+
+# このモジュールの著作権情報
+Copyright = '(c) 2018 mkht. All rights reserved.'
+
+# このモジュールの機能の説明
+# Description = ''
+
+# このモジュールに必要な Windows PowerShell エンジンの最小バージョン
+PowerShellVersion = '5.0'
+
+# Minimum version of the common language runtime (CLR) required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
+# CLRVersion = ''
+
+# RootModule/ModuleToProcess に指定されているモジュールの入れ子になったモジュールとしてインポートするモジュール
+# NestedModules = @()
+
+# このモジュールからエクスポートする関数です。最適なパフォーマンスを得るには、ワイルドカードを使用せず、エクスポートする関数がない場合は、エントリを削除しないで空の配列を使用してください。
+FunctionsToExport = @()
+
+# このモジュールからエクスポートするコマンドレットです。最適なパフォーマンスを得るには、ワイルドカードを使用せず、エクスポートするコマンドレットがない場合は、エントリを削除しないで空の配列を使用してください。
+CmdletsToExport = @()
+
+# このモジュールからエクスポートする変数
+VariablesToExport = '*'
+
+# このモジュールからエクスポートするエイリアスです。最適なパフォーマンスを得るには、ワイルドカードを使用せず、エクスポートするエイリアスがない場合は、エントリを削除しないで空の配列を使用してください。
+AliasesToExport = @()
+
+# このモジュールからエクスポートする DSC リソース
+# DscResourcesToExport = @()
+
+# このモジュールからエクスポートされたコマンドの既定のプレフィックス。既定のプレフィックスをオーバーライドする場合は、Import-Module -Prefix を使用します。
+# DefaultCommandPrefix = ''
+
+}
+
---
+++ DSCResources/cPath/cPath.psm1
@@ -0,0 +1,168 @@
+﻿<# ++++++++++++++++++++++++++++++++++++++++++++++
+Get-TargetResource
++++++++++++++++++++++++++++++++++++++++++++++++++ #>
+function Get-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Present", "Absent")]
+        [string]
+        $Ensure = 'Present',
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Value,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Machine", "User")]
+        [string]
+        $Target = 'Machine'
+    )
+    $GetRes = @{
+        Ensure = $Ensure
+        Value  = $Value
+        Target = $Target
+    }
+
+    $EnvList = Get-EnvironmentPath -Target $Target -ErrorAction Stop
+
+    if ($EnvList -eq $Value) {
+        $GetRes.Ensure = 'Present'
+    }
+    else {
+        $GetRes.Ensure = 'Absent'
+    }
+
+    $GetRes
+} # end of Get-TargetResource
+
+
+<# ++++++++++++++++++++++++++++++++++++++++++++++
+Test-TargetResource
++++++++++++++++++++++++++++++++++++++++++++++++++ #>
+function Test-TargetResource {
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Present", "Absent")]
+        [string]
+        $Ensure = 'Present',
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Value,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Machine", "User")]
+        [string]
+        $Target = 'Machine'
+    )
+
+    return ((Get-TargetResource @PSBoundParameters).Ensure -eq $Ensure)
+} # end of Test-TargetResource
+
+<# ++++++++++++++++++++++++++++++++++++++++++++++
+Set-TargetResource
++++++++++++++++++++++++++++++++++++++++++++++++++ #>
+function Set-TargetResource {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Present", "Absent")]
+        [string]
+        $Ensure = 'Present',
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Value,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Machine", "User")]
+        [string]
+        $Target = 'Machine'
+    )
+    $ErrorActionPreference = 'Stop'
+
+    if ($Ensure -eq 'Absent') {
+        #Remove Value
+        Write-Verbose ('Remove Value "{0}" from PATH' -f $Value)
+        Remove-EnvironmentPath -Path $Value -Target $Target | Out-Null
+    }
+    elseif ($Ensure -eq 'Present') {
+        #Add Value
+        Write-Verbose ('Add Value "{0}" to PATH' -f $Value)
+        Add-EnvironmentPath -Path $Value -Target $Target | Out-Null
+    }
+
+} # end of Set-TargetResource
+
+
+<# ++++++++++++++++++++++++++++++++++++++++++++++
+環境変数PATHの値を取得する
++++++++++++++++++++++++++++++++++++++++++++++++++ #>
+function Get-EnvironmentPath {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, Position = 1)]
+        [ValidateSet("User", "Machine")]
+        [string]$Target # UserかMachineか選ぶ
+    )
+
+    $PathEnv = New-Object System.Collections.ArrayList
+    ([System.Environment]::GetEnvironmentVariable("Path", $Target)) -split ';' | ForEach-Object {$PathEnv.Add($_)} | Out-Null
+    $PathEnv
+}
+
+<# ++++++++++++++++++++++++++++++++++++++++++++++
+環境変数PATHから指定の値を削除する
++++++++++++++++++++++++++++++++++++++++++++++++++ #>
+function Remove-EnvironmentPath {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $Path, # 削除する値
+
+        [Parameter(Mandatory = $true, Position = 1)]
+        [ValidateSet("User", "Machine")]
+        [string]$Target # UserかMachineか選ぶ
+    )
+
+    $PathEnv = New-Object System.Collections.ArrayList
+    ([System.Environment]::GetEnvironmentVariable("Path", $Target)) -split ';' | ForEach-Object {$PathEnv.Add($_)} | Out-Null
+    if ($PathEnv -contains $Path) {
+        $PathEnv = ($PathEnv -ne $Path)
+        [System.Environment]::SetEnvironmentVariable("Path", ($PathEnv -join ';'), $Target)
+    }
+    [System.Environment]::GetEnvironmentVariable("Path", $Target)
+}
+
+<# ++++++++++++++++++++++++++++++++++++++++++++++
+環境変数PATHの末尾に指定の値を追加する
++++++++++++++++++++++++++++++++++++++++++++++++++ #>
+function Add-EnvironmentPath {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $Path, # 追加する値
+
+        [Parameter(Mandatory = $true, Position = 1)]
+        [ValidateSet("User", "Machine")]
+        [string]$Target # UserかMachineか選ぶ
+    )
+
+    $PathEnv = New-Object System.Collections.ArrayList
+    ([System.Environment]::GetEnvironmentVariable("Path", $Target)) -split ';' | ForEach-Object {$PathEnv.Add($_)} | Out-Null
+    if ($PathEnv -notcontains $Path) {
+        $PathEnv.Add($Path)
+        [System.Environment]::SetEnvironmentVariable("Path", ($PathEnv -join ';'), $Target)
+    }
+    [System.Environment]::GetEnvironmentVariable("Path", $Target)
+}
+
+# ////////////////////////////////////////////////////////////////////////////////////////
+Export-ModuleMember -Function *-TargetResource
